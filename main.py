9049075898229f0f2a2facb75dc1b61eb70b9226
Aleksandr R. <redalnik@gmail.com>
2025-08-10 01:22:02 +0300

    add test_widget.py, fixed widget.py

--- main.py
+++ main.py
@@ -4,20 +4,6 @@ from src.processing import sort_by_date
 from src.widget import get_date
 from src.widget import mask_account_card
 
-print(get_mask_card_number("1234567891234567"))
-
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(mask_account_card("Visa Platinum 7000792289606361"))
-
-print(get_date("2024-03-11T02:26:18.671407"))
-
 print(
     sort_by_date(
         [
@@ -62,19 +48,3 @@ print(
 #         ], False
 #     )
 #     print(date_list)
-
-
-
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-
-if __name__ == "__main__":
-    print(get_mask_account("73654108430135874305"))
-
-if __name__ == "__main__":
-    time = get_date("2024-03-11T02:26:18.671407")
-    print(time)
-
-if __name__ == "__main__":
-    q = mask_account_card("Счет 73654108430135874305")
-    print(q)
--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,25 @@
+import datetime
 from src.masks import get_mask_account
 from src.masks import get_mask_card_number
 
 
 def mask_account_card(user_details: str) -> str:
     """Функция принимает информацию о карте или счете и возвращает строку с замаскированным номером"""
-    account_number = ""
-    account_type = ""
-    for i in user_details:
-        if i.isdigit():
-            account_number += i
-        else:
-            account_type += i
+    account_number = "".join([letter for letter in user_details if letter.isdigit()])
+    account_type = " ".join([word for word in user_details.split() if all(letter.isalpha() for letter in word)])
     if len(account_number) == 16:
-        return f"{account_type}{get_mask_card_number(account_number)}"
+        return f"{account_type} {get_mask_card_number(account_number)}"
+    elif len(account_number) == 20:
+        return f"{account_type} {get_mask_account(account_number)}"
     else:
-        return f"{account_type}{get_mask_account(account_number)}"
+        return "Неверный номер счета/карты"
 
 
 def get_date(received_date: str) -> str:
-    """Функция получает строку с датой и приводит ее к формату: "ДД.ММ.ГГГГ" """
-    format_date = f"{received_date[8:10]}.{received_date[5:7]}.{received_date[:4]}"
-    return format_date
+    """Функция принимает строку с датой в формате '2024-03-11T02:26:18.671407'
+    и возвращает дату в формате 'ДД.ММ.ГГГГ'"""
+    try:
+        date_obj = datetime.datetime.fromisoformat(received_date)
+        return date_obj.strftime("%d.%m.%Y")
+    except (ValueError, TypeError):
+        return "Некорректный формат даты"
